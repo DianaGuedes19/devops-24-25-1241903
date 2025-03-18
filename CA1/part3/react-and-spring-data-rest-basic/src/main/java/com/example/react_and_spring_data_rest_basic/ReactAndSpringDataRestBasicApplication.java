5f0ac90a34c5f7097174f81d913185f62cf5bb03
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-18 14:16:32 +0000

    Added springboot initialzr folder

---
+++ CA1/part3/react-and-spring-data-rest-basic/src/main/java/com/example/react_and_spring_data_rest_basic/ReactAndSpringDataRestBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_and_spring_data_rest_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestBasicApplication.class, args);
+	}
+
+}
---
+++ CA1/part3/react-and-spring-data-rest-basic/src/test/java/com/example/react_and_spring_data_rest_basic/ReactAndSpringDataRestBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_and_spring_data_rest_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringDataRestBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
